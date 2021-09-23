60c42c1f142c909d3c17676393ec8bd8f7047343
Koketso-dax <lanchaster.k@gmail.com>
2021-09-23 18:52:39 +0200

    included test code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
-
+import React, {useState}from "react";
+import  { getAyoba }  from './microapp'
+import "./App.css" ;
 
 function App() {
-  return (
-    <div className="App">
 
-    
-    </div>
-  );
+  const [phone, setPhone] = useState('');
+
+
+  const sayHello = () => {
+
+   
+    let ayoba = getAyoba() ; 
+    setPhone(ayoba.getMsisdn());
+
+  } 
+
+  return (
+    <div className="App">    
+      <h1>Hello React</h1>
+  <p>MSISDN : {phone}</p>
+       <button onClick={sayHello}>
+      Click me!
+    </button>
+      </div>
+  )
 }
 
-export default App;
+export default App
